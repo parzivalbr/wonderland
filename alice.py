b02d27f17851f676e8416266d0bd6c12f1555098
parzivalbruh <yahliraviv09@gmail.com>
2026-03-11 15:39:50 +0200

    changed script to route both ways

--- alice.py
+++ alice.py
@@ -5,17 +5,15 @@ IF1 = "enp0s8"
 IF2 = "enp0s9"
 
 
-def create_socket(iface_to_sock: str):
-    return scapy.conf.L2socket(iface = iface_to_sock)
+def handle_packet(packet: scapy.packet) -> None:
+    if packet.sniffed_on == IF1:
+        scapy.sendp(packet, iface=IF2)
+    else:
+        scapy.sendp(packet, iface=IF1)
 
 
 def main() -> None:
-    s1 = create_socket(IF1)
-    s2 = create_socket(IF2)
-    while(True):
-        packet = s1.recv()
-        if packet:
-            s2.send(packet)
+    scapy.sniff(iface=[IF2, IF1], filter="ip and inbound", prn=handle_packet)
 
 
 if __name__ == "__main__":
