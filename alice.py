445af5a47c4fc437e6ffaeac8f406c39785c238d
parzivalbruh <yahliraviv09@gmail.com>
2026-03-11 16:33:40 +0200

    add subnet check, mac change and ttl dec

--- alice.py
+++ alice.py
@@ -3,17 +3,19 @@ import scapy.all as scapy
 
 IF1 = "enp0s8"
 IF2 = "enp0s9"
+IFACE2_SUBNET = "192.168.56"
+IF2_MAC_ADDR = "08:00:27:f2:2c:d3"
 
 
 def handle_packet(packet: scapy.packet) -> None:
-    if packet.sniffed_on == IF1:
+    if IFACE2_SUBNET in packet[scapy.IP].dst:
+        packet[scapy.IP].ttl -= 1
+        packet.src = IF2_MAC_ADDR
         scapy.sendp(packet, iface=IF2)
-    else:
-        scapy.sendp(packet, iface=IF1)
 
 
 def main() -> None:
-    scapy.sniff(iface=[IF2, IF1], filter="ip and inbound", prn=handle_packet)
+    scapy.sniff(iface=IF1, filter="ip and inbound", prn=handle_packet)
 
 
 if __name__ == "__main__":
